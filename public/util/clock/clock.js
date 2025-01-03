917fe4c63e68a31b1d8612c95201cae63e383e77
Jose Miguel Fierro <j.fierro.habaluyas@accenture.com>
2025-01-03 14:30:01 +0100

    Extracted utility functions to common.js

--- public/util/clock/clock.js
+++ public/util/clock/clock.js
@@ -1,5 +1,6 @@
-import * as luxon from 'https://cdn.jsdelivr.net/npm/luxon@3.5.0/+esm'
-import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@3.5.0/+esm'
+import * as luxon from 'https://cdn.jsdelivr.net/npm/luxon@3.5.0/+esm';
+import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@3.5.0/+esm';
+import { getKnownElementById, queryKnownSelector, castNode } from '../../common.js';
 
 ///////////////////////////////////////////////////////////////////////////////
 //                             Exported Globals                              //
@@ -11,78 +12,6 @@ window.luxon ??= luxon;
  * @typedef {{ UTC: string, [clockName: string]: string }} ClockDefinitons
  * Key -> Clock name, Value -> Clock offset (can be in format: UTC+01:00, EST, or America/New_York)
  */
-
-/**
- * @template {HTMLElement} TType
- * @param {string} elementId
- * @param {{ new(): TType; name: string; }} [type]
- * @returns {TType}
- */
-function getKnownElementById(elementId, type) {
-	const element = document.getElementById(elementId);
-	if (!element) {
-		throw new ReferenceError(`Element with id "${elementId}" was expected in document, but was missing.`)
-	}
-
-	if (type && !(element instanceof type)) {
-		throw new TypeError(`Element with id "${elementId}" was expected to be of type ${type.name} but was actually of type ${element.constructor?.name}`);
-	}
-
-	// @ts-ignore
-	return element;
-}
-
-/**
- * @template {HTMLElement} TType
- * @param {HTMLElement | ParentNode} parent
- * @param {string} selector
- * @param {{ new(): TType; name: string; }} [type]
- * @returns {TType}
- */
-function queryKnownSelector(parent, selector, type) {
-	const element = parent.querySelector(selector);
-
-	if (!element) {
-		let parentName;
-		if (parent === document) {
-			parentName = "document";
-		}
-		else if ('classList' in parent) {
-			if (parent.id) {
-				parentName = parent.id;
-			}
-			else {
-				parentName = parent.tagName;
-				if (parent.classList.length) {
-					parentName = `${parentName} ${parent.classList}`;
-				}
-			}
-		}
-		throw new ReferenceError(`Element with selector "${selector}" was expected under ${parentName}, but was missing.`)
-	}
-
-	if (type && !(element instanceof type)) {
-		throw new TypeError(`Element with selector "${selector}" was expected to be of type ${type.name} but was actually of type ${element.constructor?.name}`);
-	}
-
-	// @ts-ignore
-	return element;
-}
-
-/**
- * @template {Node} TType
- * @param {{ new(): TType; name: string; }} type
- * @param {Node} node
- * @returns {TType}
- */
-function castNode(type, node) {
-	if (type && !(node instanceof type)) {
-		throw new TypeError(`Node "${node}" was expected to be of type ${type.name} but was actually of type ${node.constructor?.name}`);
-	}
-
-	// @ts-ignore
-	return node;
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                   Logic                                   //
